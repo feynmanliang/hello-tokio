8f483eb44ef7f8e9a47157bba9499a6bd0249409
Feynman Liang <feynman.liang@gmail.com>
2023-03-17 18:53:30 +0000

    Shared db with mutex

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
 use tokio::net::TcpListener;
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
 
+        println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: tokio::net::TcpStream) {
+async fn process(socket: tokio::net::TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
 
@@ -27,13 +36,17 @@ async fn process(socket: tokio::net::TcpStream) {
 
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
-            Get(cmd) => match db.get(cmd.key()) {
-                Some(value) => Frame::Bulk(value.clone().into()),
-                None => Frame::Null,
-            },
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                match db.get(cmd.key()) {
+                    Some(value) => Frame::Bulk(value.clone()),
+                    None => Frame::Null,
+                }
+            }
             cmd => unimplemented!("unimplemented command: {:?}", cmd),
         };
 
