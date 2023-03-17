acc92adee2032c9fc1aaab0ecde83603c41456f4
Feynman Liang <feynman.liang@gmail.com>
2023-03-17 22:35:39 +0000

    Echo num bytes copied on EOF

--- src/bin/echo-server-copy.rs
+++ src/bin/echo-server-copy.rs
@@ -11,7 +11,9 @@ async fn main() -> io::Result<()> {
         tokio::spawn(async move {
             let (mut rd, mut wr) = socket.split();
 
-            if io::copy(&mut rd, &mut wr).await.is_err() {
+            if let Ok(num_bytes) = io::copy(&mut rd, &mut wr).await {
+                println!("Copied {num_bytes} bytes");
+            } else {
                 eprintln!("failed to copy");
             }
         });
