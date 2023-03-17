3bb1f597b36278ff0f3b1ecffbd3bab8a931b43a
Feynman Liang <feynman.liang@gmail.com>
2023-03-17 17:56:47 +0000

    Implements socket listener

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-    println!("got value from the server; result={:?}", result);
-    Ok(())
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: tokio::net::TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
+
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
