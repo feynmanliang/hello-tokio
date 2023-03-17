5259dcfff7537c70b3c0229da4c4a4fa5434ec5a
Feynman Liang <feynman.liang@gmail.com>
2023-03-17 23:33:42 +0000

    Adds framing

---
+++ src/connection.rs
@@ -0,0 +1,113 @@
+use std::io::Cursor;
+
+use bytes::{Buf, BytesMut};
+use mini_redis::{Frame, Result};
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt, BufWriter},
+    net::TcpStream,
+};
+
+pub struct Connection {
+    stream: BufWriter<TcpStream>,
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream: BufWriter::new(stream),
+            buffer: BytesMut::with_capacity(4096),
+        }
+    }
+
+    /// Read a frame from the connection.
+    ///
+    /// Returns `None` if EOF is reached
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            // 0 indicates EOF
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    // partial frame indicates abrupt termination
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
+    }
+
+    /// Write a frame to the connection.
+    pub async fn write_frame(&mut self, frame: &Frame) -> Result<()> {
+        match frame {
+            Frame::Simple(val) => {
+                self.stream.write_u8(b'+').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Error(val) => {
+                self.stream.write_u8(b'-').await?;
+                self.stream.write_all(val.as_bytes()).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Integer(val) => {
+                self.stream.write_u8(b':').await?;
+                self.write_decimal(*val).await?;
+            }
+            Frame::Null => {
+                self.stream.write_all(b"$-1\r\n").await?;
+            }
+            Frame::Bulk(val) => {
+                let len = val.len();
+
+                self.stream.write_u8(b'$').await?;
+                self.write_decimal(len as u64).await?;
+                self.stream.write_all(val).await?;
+                self.stream.write_all(b"\r\n").await?;
+            }
+            Frame::Array(_val) => unimplemented!(),
+        }
+
+        self.stream.flush().await?;
+
+        Ok(())
+    }
+
+    fn parse_frame(&mut self) -> Result<Option<Frame>> {
+        let mut buf = Cursor::new(&self.buffer[..]);
+
+        // Ensure a full frame is buffered and find the end index of the frame.
+        match Frame::check(&mut buf) {
+            Ok(_) => {
+                // Parse the frame.
+                let len = buf.position() as usize;
+                buf.set_position(0);
+                let frame = Frame::parse(&mut buf)?;
+                self.buffer.advance(len);
+                Ok(Some(frame))
+            }
+            Err(mini_redis::frame::Error::Incomplete) => Ok(None),
+            Err(e) => Err(e.into()),
+        }
+    }
+
+    /// Write a decimal frame to the stream
+    async fn write_decimal(&mut self, val: u64) -> io::Result<()> {
+        use std::io::Write;
+
+        // Convert the value to a string
+        let mut buf = [0u8; 12];
+        let mut buf = Cursor::new(&mut buf[..]);
+        write!(&mut buf, "{}", val)?;
+
+        let pos = buf.position() as usize;
+        self.stream.write_all(&buf.get_ref()[..pos]).await?;
+        self.stream.write_all(b"\r\n").await?;
+
+        Ok(())
+    }
+}
